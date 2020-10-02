00a8b01c5c51e9acd3b4daff5ac6619aed01b482
Amaresh Pattanaik <amaresh.pattanaik@workday.com>
2020-10-01 19:44:22 -0700

    version 1

---
+++ mysamples/pandas_examples.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+
+oecd_bli = pd.read_csv("oecd_bli_2015.csv", thousands=',')
+gdp_per_capita = pd.read_csv("gdp_per_capita.csv", thousands=',', delimiter='\t', encoding='latin1', na_values="n/a")
+
+print(oecd_bli.shape)
